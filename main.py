28c524b9e680c8003524b5bfb42cff8c755c9d59
realko4n <90069252+realko4n@users.noreply.github.com>
2025-04-18 17:13:56 +0300

    gaz

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QMessageBox
+
+class TicTacToe(QWidget):
+    def __init__(self):
+        super().__init__()
